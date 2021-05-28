97b64e4bbb0de79f477f25507f6d25223d9a2cec
Jorge Morfinez Mojica <jorge.morfinez.m@gmail.com>
2021-05-27 22:01:11 -0500

    edit name of schemas

--- urbvan_dll_db_microservice_test.sql
+++ urbvan_dll_db_microservice_test.sql
@@ -1,10 +1,10 @@
--- DROP SCHEMA urbvan;
+-- DROP SCHEMA van;
 
-CREATE SCHEMA urbvan AUTHORIZATION postgres;
+CREATE SCHEMA van AUTHORIZATION postgres;
 
--- DROP TYPE urbvan."_user_auth_api";
+-- DROP TYPE van."_user_auth_api";
 
-CREATE TYPE urbvan."_user_auth_api" (
+CREATE TYPE van."_user_auth_api" (
 	INPUT = array_in,
 	OUTPUT = array_out,
 	RECEIVE = array_recv,
@@ -13,12 +13,12 @@ CREATE TYPE urbvan."_user_auth_api" (
 	ALIGNMENT = 8,
 	STORAGE = any,
 	CATEGORY = A,
-	ELEMENT = urbvan.user_auth_api,
+	ELEMENT = van.user_auth_api,
 	DELIMITER = ',');
 
--- DROP TYPE urbvan."_van_vehicle";
+-- DROP TYPE van."_van_vehicle";
 
-CREATE TYPE urbvan."_van_vehicle" (
+CREATE TYPE van."_van_vehicle" (
 	INPUT = array_in,
 	OUTPUT = array_out,
 	RECEIVE = array_recv,
@@ -27,12 +27,12 @@ CREATE TYPE urbvan."_van_vehicle" (
 	ALIGNMENT = 8,
 	STORAGE = any,
 	CATEGORY = A,
-	ELEMENT = urbvan.van_vehicle,
+	ELEMENT = van.van_vehicle,
 	DELIMITER = ',');
 
--- DROP TYPE urbvan.user_auth_api;
+-- DROP TYPE van.user_auth_api;
 
-CREATE TYPE urbvan.user_auth_api AS (
+CREATE TYPE van.user_auth_api AS (
 	user_id numeric,
 	username varchar,
 	"password" varchar,
@@ -40,9 +40,9 @@ CREATE TYPE urbvan.user_auth_api AS (
 	creation_date timestamp,
 	last_update_date timestamp);
 
--- DROP TYPE urbvan.van_vehicle;
+-- DROP TYPE van.van_vehicle;
 
-CREATE TYPE urbvan.van_vehicle AS (
+CREATE TYPE van.van_vehicle AS (
 	uuid_van uuid,
 	plates_van varchar,
 	economic_number_van varchar,
@@ -51,9 +51,9 @@ CREATE TYPE urbvan.van_vehicle AS (
 	status_van varchar,
 	last_update_date timestamp);
 
--- DROP SEQUENCE urbvan.eco_num_van;
+-- DROP SEQUENCE van.eco_num_van;
 
-CREATE SEQUENCE urbvan.eco_num_van
+CREATE SEQUENCE van.eco_num_van
 	INCREMENT BY 1
 	MINVALUE 1
 	MAXVALUE 9223372036854775807
@@ -63,15 +63,15 @@ CREATE SEQUENCE urbvan.eco_num_van
 
 -- Permissions
 
-ALTER SEQUENCE urbvan.eco_num_van OWNER TO postgres;
-GRANT ALL ON SEQUENCE urbvan.eco_num_van TO postgres;
+ALTER SEQUENCE van.eco_num_van OWNER TO postgres;
+GRANT ALL ON SEQUENCE van.eco_num_van TO postgres;
 -- urbvan.user_auth_api definition
 
 -- Drop table
 
--- DROP TABLE urbvan.user_auth_api;
+-- DROP TABLE van.user_auth_api;
 
-CREATE TABLE urbvan.user_auth_api (
+CREATE TABLE van.user_auth_api (
 	user_id numeric NOT NULL,
 	username varchar NOT NULL, -- Username to authenticate to the API
 	"password" varchar NOT NULL, -- Password to authenticate to API
@@ -80,27 +80,27 @@ CREATE TABLE urbvan.user_auth_api (
 	last_update_date timestamp(0) NULL,
 	CONSTRAINT user_auth_api_pk PRIMARY KEY (user_id)
 );
-COMMENT ON TABLE urbvan.user_auth_api IS 'contain the data to authenticate to the API';
+COMMENT ON TABLE van.user_auth_api IS 'contain the data to authenticate to the API';
 
 -- Column comments
 
-COMMENT ON COLUMN urbvan.user_auth_api.username IS 'Username to authenticate to the API';
-COMMENT ON COLUMN urbvan.user_auth_api."password" IS 'Password to authenticate to API';
-COMMENT ON COLUMN urbvan.user_auth_api.password_hash IS 'Password hash authenticated';
+COMMENT ON COLUMN van.user_auth_api.username IS 'Username to authenticate to the API';
+COMMENT ON COLUMN van.user_auth_api."password" IS 'Password to authenticate to API';
+COMMENT ON COLUMN van.user_auth_api.password_hash IS 'Password hash authenticated';
 
 -- Permissions
 
-ALTER TABLE urbvan.user_auth_api OWNER TO postgres;
-GRANT ALL ON TABLE urbvan.user_auth_api TO postgres;
+ALTER TABLE van.user_auth_api OWNER TO postgres;
+GRANT ALL ON TABLE van.user_auth_api TO postgres;
 
 
--- urbvan.van_vehicle definition
+-- van.van_vehicle definition
 
 -- Drop table
 
--- DROP TABLE urbvan.van_vehicle;
+-- DROP TABLE van.van_vehicle;
 
-CREATE TABLE urbvan.van_vehicle (
+CREATE TABLE van.van_vehicle (
 	uuid_van uuid NOT NULL, -- UUID unico para identificar la reserva del usuario
 	plates_van varchar NOT NULL, -- Placas de la VAN
 	economic_number_van varchar NOT NULL, -- Una nomenclatura para identificar a las camionetas.Ejemplo A1-0001
@@ -112,26 +112,26 @@ CREATE TABLE urbvan.van_vehicle (
 	CONSTRAINT van_vehicle_pk PRIMARY KEY (uuid_van),
 	CONSTRAINT van_vehicle_un UNIQUE (uuid_van, plates_van)
 );
-COMMENT ON TABLE urbvan.van_vehicle IS 'contain the van vehicle attributes to manage it';
+COMMENT ON TABLE van.van_vehicle IS 'contain the van vehicle attributes to manage it';
 
 -- Column comments
 
-COMMENT ON COLUMN urbvan.van_vehicle.uuid_van IS 'UUID unico para identificar la reserva del usuario';
-COMMENT ON COLUMN urbvan.van_vehicle.plates_van IS 'Placas de la VAN';
-COMMENT ON COLUMN urbvan.van_vehicle.economic_number_van IS 'Una nomenclatura para identificar a las camionetas.Ejemplo A1-0001';
-COMMENT ON COLUMN urbvan.van_vehicle.seats_van IS 'Cantidad de asientos disponibles por camioneta';
-COMMENT ON COLUMN urbvan.van_vehicle.created_at IS 'Fecha de alta de la VAN';
-COMMENT ON COLUMN urbvan.van_vehicle.status_van IS 'Posibles estatus de la VAN: “Activa”, “En reparación”, “Baja”';
-COMMENT ON COLUMN urbvan.van_vehicle.last_update_date IS 'Contiene la fecha de actualizacion del registro';
+COMMENT ON COLUMN van.van_vehicle.uuid_van IS 'UUID unico para identificar la reserva del usuario';
+COMMENT ON COLUMN van.van_vehicle.plates_van IS 'Placas de la VAN';
+COMMENT ON COLUMN van.van_vehicle.economic_number_van IS 'Una nomenclatura para identificar a las camionetas.Ejemplo A1-0001';
+COMMENT ON COLUMN van.van_vehicle.seats_van IS 'Cantidad de asientos disponibles por camioneta';
+COMMENT ON COLUMN van.van_vehicle.created_at IS 'Fecha de alta de la VAN';
+COMMENT ON COLUMN van.van_vehicle.status_van IS 'Posibles estatus de la VAN: “Activa”, “En reparación”, “Baja”';
+COMMENT ON COLUMN van.van_vehicle.last_update_date IS 'Contiene la fecha de actualizacion del registro';
 
 -- Permissions
 
-ALTER TABLE urbvan.van_vehicle OWNER TO postgres;
-GRANT ALL ON TABLE urbvan.van_vehicle TO postgres;
+ALTER TABLE van.van_vehicle OWNER TO postgres;
+GRANT ALL ON TABLE van.van_vehicle TO postgres;
 
 
 
 
 -- Permissions
 
-GRANT ALL ON SCHEMA urbvan TO postgres;
+GRANT ALL ON SCHEMA van TO postgres;
